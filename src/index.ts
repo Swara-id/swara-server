c9270956378ba409a4863d5d6fabdd527b6f99f9
ahmadsultani <ahmadsultanidayanullah@gmail.com>
2024-06-08 08:14:32 +0800

    chore: prettier rule

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
-import 'dotenv/config';
-import '@config/firebase-config';
+import "dotenv/config";
+import "@config/firebase-config";
 
-import express, { Request, Response, urlencoded } from 'express';
-import morgan from 'morgan';
-import swaggerUi from 'swagger-ui-express';
+import express, { Request, Response, urlencoded } from "express";
+import morgan from "morgan";
+import swaggerUi from "swagger-ui-express";
 
-import corpusRouter from '@api/corpus/route';
-import usersRouter from '@api/users/route';
-import quizRouter from '@api/quiz/route';
-import newsRouter from '@api/news/route';
-import suggestionRouter from '@api/suggestion/route';
-import eventTypeRouter from '@api/newsType/route';
-import challengeTypeRouter from '@api/Challenge/route';
-import authRouter from '@api/auth/route';
+import corpusRouter from "@api/corpus/route";
+import usersRouter from "@api/users/route";
+import quizRouter from "@api/quiz/route";
+import newsRouter from "@api/news/route";
+import suggestionRouter from "@api/suggestion/route";
+import eventTypeRouter from "@api/newsType/route";
+import challengeTypeRouter from "@api/Challenge/route";
+import authRouter from "@api/auth/route";
 
 const app = express();
 const port = process.env.PORT || 3000;
-const route = 'api/v1';
+const route = "api/v1";
 
 app.use(
   urlencoded({
@@ -24,20 +24,22 @@ app.use(
   }),
 );
 app.use(express.json());
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
+app.use(express.static("build"));
 
 app.use(
-  '/docs',
+  "/docs",
   swaggerUi.serve,
   swaggerUi.setup(undefined, {
+    swaggerUrl: "swagger.json",
     swaggerOptions: {
-      url: './swagger.json',
+      validatorUrl: null,
     },
   }),
 );
 
 app.get(`/${route}/health`, (_req: Request, res: Response) => {
-  res.send('Hello, TypeScript Express!');
+  res.send("Hello, TypeScript Express!");
 });
 
 app.use(`/${route}/corpus`, corpusRouter);
